b9bedcb1b6d4d195f187997cf29d82f2229bfd5e
NASH021 <38508275+NASH021@users.noreply.github.com>
2018-08-28 19:03:43 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+  $(document).ready(function() {
+            $(".menu-icon").click(function() {
+                $(".menu").toggle();
+            });
+        });
+
+
+
+  $(function () {
+  $('[data-toggle="popover"]').popover()
+});
+
